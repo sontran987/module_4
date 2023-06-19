aa6f2e9bf91a9ae99e6c2d978e1252228874c538
Tran Thanh Son <sonva2002@gmail.com>
2023-06-19 10:44:46 +0700

    up

--- ss2/exercise_may_tinh_ca_nhan/src/main/java/com/example/exercise_may_tinh_ca_nhan/controller/PersonalComputerController.java
+++ ss2/exercise_may_tinh_ca_nhan/src/main/java/com/example/exercise_may_tinh_ca_nhan/controller/PersonalComputerController.java
@@ -21,13 +21,18 @@ public class PersonalComputerController {
 
     @PostMapping("/calculate")
     public String computer(@RequestParam(value = "number1", required = false) Double number1, @RequestParam(value = "number2", required = false) Double number2,
-                           @RequestParam(value = "calculate",required = false) String calculate, RedirectAttributes redirectAttributes) {
+                           @RequestParam(value = "calculate", required = false) String calculate, RedirectAttributes redirectAttributes) {
         String message = "Result: ";
-        if (number1 == null || number2 == null || calculate == ""){
+        if (number2 == 0.0 && calculate.equals("divi")) {
+            message = "No result";
+            redirectAttributes.addFlashAttribute("result", message);
             return "redirect:/";
         }
-        double result = conputerService.calculate(number1,number2,calculate);
-        redirectAttributes.addFlashAttribute("result",message+result);
+        if (number1 == null || number2 == null || calculate == "") {
+            return "redirect:/";
+        }
+        double result = conputerService.calculate(number1, number2, calculate);
+        redirectAttributes.addFlashAttribute("result", message + result);
         return "redirect:/";
     }
 }
