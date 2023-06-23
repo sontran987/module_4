e427d33c13d096b2fb93f6a15e88aea549b3f498
Tran Thanh Son <sonva2002@gmail.com>
2023-06-23 11:04:41 +0700

    upup

--- ss7/execrise_blog_appp/src/main/java/com/example/execrise_blog_appp/model/repository/IBlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.execrise_blog_appp.model.repository;
-
-import com.example.execrise_blog_appp.model.model.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBlogRepository extends JpaRepository<Blog,Integer> {
-
-}
